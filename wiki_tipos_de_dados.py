e832f33f68a04d3637b67c80a6fa06345dbcf642
i820244 <rodrigo.buhler@sap.com>
2023-07-15 18:27:38 -0300

    Atualizacao

---
+++ wiki_tipos_de_dados.py
@@ -0,0 +1,18 @@
+# TIPOS DE DADOS
+# https://www.w3schools.com/python/python_datatypes.asp
+
+print("Tipos de dados")
+print("Lista")
+# LISTA - LIST
+lista = []
+lista = ["item1", "item2", "item3"]
+
+for item_lista in lista:
+    print("Item : ", item_lista)
+
+
+print("Dicionario")
+# DICIONARIO - DICTIONARY
+dicionario = {}
+dicionario = {"item1": "valor item 1", "item2": 99, "item3" : 123.90}
+
